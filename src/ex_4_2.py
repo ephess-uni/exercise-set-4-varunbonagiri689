39dda00e41719750f00585219e9279d1a9efbd86
varunbonagiri689 <149261125+varunbonagiri689@users.noreply.github.com>
2023-12-09 06:41:27 +0530

    Update ex_4_2.py

--- src/ex_4_2.py
+++ src/ex_4_2.py
@@ -6,8 +6,9 @@ def logstamp_to_datetime(datestr):
     """
     Your docstring here.  Replace the pass keyword below with your implementation.
     """
-    pass
-
+    bad_guy = datestr
+    good_guy = '%Y-%m-%dT%H:%M:%S'
+    return datetime.strptime(bad_guy, good_guy)
 
 # >>>> The code below will call your function and print the results
 if __name__ == "__main__":
